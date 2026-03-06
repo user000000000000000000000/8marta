10fa40f392ad002050e02d54b69ecbc15c78b510
Антонов Иван <144132631+user000000000000000000000@users.noreply.github.com>
2026-03-06 04:46:43 +0300

    пробуем 3 раз

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/8marta">
       <div className="app">
         <HeartBackground />
         <nav className="nav">
