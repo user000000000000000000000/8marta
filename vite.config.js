56107d4eda760da09e8d5d47dcc1f666dd568153
Антонов Иван <144132631+user000000000000000000000@users.noreply.github.com>
2026-03-06 04:28:02 +0300

    Для Юлечки

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/8marta/', // ВАЖНО: ТОЧНОЕ НАЗВАНИЕ ТВОЕГО РЕПОЗИТОРИЯ!
+  build: {
+    outDir: 'docs' // Будем собирать в папку docs (GitHub Pages любит её)
+  }
 })
